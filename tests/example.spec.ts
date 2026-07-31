5d212bcda10ff986d573d568c41f008907e892ae
Mitzi Mejia <mitzi.mejia.89@gmail.com>
2026-07-31 17:16:06 -0600

    Semana 2: test de login automatizado en SauceDemo

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// This test validates the login on saucedemo.com page, 1st practice week 2
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test('Saucedemo page is displayed', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+
+  // Expect a button for "Login" to be visible on the page.
+  await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+});
+ 
+test('Login into Saucedemo page', async({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.getByPlaceholder('Username').fill('standard_user');
+  await page.getByPlaceholder('Password').fill('secret_sauce');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByText('Swag Labs')).toBeVisible();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
